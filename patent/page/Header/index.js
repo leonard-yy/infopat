038fd5d62f4878084ba7ec7d3982c1a7b523e3ff
刘刚 <0027011700@iwhalecloud.com>
2021-02-05 11:49:04 +0800

    fixed:登录用户信息

--- patent/page/Header/index.js
+++ patent/page/Header/index.js
@@ -1,11 +1,11 @@
 (function initPage() {
-  layui.use(
-    ["request", "patNodataPage", "loader"],
-    function () {
-      var $ = layui.jquery,
-        request = layui.request;
+  layui.use(["request", "patNodataPage", "loader"], function () {
+    var $ = layui.jquery,
+      request = layui.request;
 
-      request.get(`/api/user/session?t=${new Date().getTime()}`, function (result) {
+    request.get(
+      `/api/user/session?t=${new Date().getTime()}`,
+      function (result) {
         if (result && result.error == 0) {
           $(".username").html(result.data.username);
           if (result.data.is_trial_period) {
@@ -14,10 +14,10 @@
         } else {
           $(".period-tips").hide();
         }
-      });
-    },
-    function (err) {
-      $(".period-tips").hide();
-    }
-  );
+      },
+      function (err) {
+        $(".period-tips").hide();
+      }
+    );
+  });
 })();
